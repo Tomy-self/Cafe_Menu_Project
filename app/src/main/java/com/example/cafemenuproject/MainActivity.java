0dc0ff7d8eba00dcd01425da77613edf2f73e3da
Tomy-self <andywow2364@naver.com>
2022-06-30 12:02:41 +0900

    6/30

--- app/src/main/java/com/example/cafemenuproject/MainActivity.java
+++ app/src/main/java/com/example/cafemenuproject/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.cafemenuproject;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    Button btn1, btn2, btn3, btn4;
+    TextView textResult;
+
+    String num1, num2;
+    Integer result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("초간단 계산기");
+
+        edit1=(EditText) findViewById(R.id.Edit1);
+        edit2=(EditText) findViewById(R.id.Edit2);
+
+        btn1=(Button) findViewById((R.id.Btn1));
+        btn2=(Button) findViewById((R.id.Btn2));
+        btn3=(Button) findViewById((R.id.Btn3));
+        btn4=(Button) findViewById((R.id.Btn4));
+
+        textResult=(TextView) findViewById((R.id.TextResult));
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1=edit1.getText().toString();
+                num2=edit2.getText().toString();
+                result=Integer.parseInt(num1)+Integer.parseInt(num2);
+                textResult.setText("계산 결과: "+result.toString());
+            }
+        });
+
+        btn2.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1=edit1.getText().toString();
+                num2=edit2.getText().toString();
+                result=Integer.parseInt(num1)-Integer.parseInt(num2);
+                textResult.setText("계산 결과: "+result.toString());
+            }
+        }));
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1=edit1.getText().toString();
+                num2=edit2.getText().toString();
+                result=Integer.parseInt(num1)*Integer.parseInt(num2);
+                textResult.setText("계산 결과: "+result.toString());
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1=edit1.getText().toString();
+                num2=edit2.getText().toString();
+                result=Integer.parseInt(num1)/Integer.parseInt(num2);
+                textResult.setText("계산 결과: "+result.toString());
+            }
+        });
     }
 }
